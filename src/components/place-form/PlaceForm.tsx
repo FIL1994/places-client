4c25b12d4ee5075d33e80ab5ef8aed1c5dd7b8dc
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-10-05 13:52:39 -0400

    added forms to field

--- src/components/place-form/PlaceForm.tsx
+++ src/components/place-form/PlaceForm.tsx
@@ -1,11 +1,50 @@
 import * as React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import "./place-form.less";
 
 const PlaceForm: React.FunctionComponent = () => {
+  const [title, setTitle] = React.useState("");
+  const [address, setAddress] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+
   return (
-    <div>
-      <h3>Add Place</h3>
-      <form>
-        <div />
+    <div className="place-form">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log("submit");
+        }}
+      >
+        <TextField
+          autoFocus
+          label="Title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <TextField
+          label="Address"
+          value={address}
+          onChange={e => setAddress(e.target.value)}
+          multiline
+          rows="2"
+        />
+        <TextField
+          label="Image URL"
+          value={imageUrl}
+          onChange={e => setImageUrl(e.target.value)}
+        />
+        <TextField
+          label="Description"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+          multiline
+          rows="4"
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Add Place
+        </Button>
       </form>
     </div>
   );
